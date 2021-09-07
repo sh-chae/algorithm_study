c740cbbc40bcdc0b7c028e6b36d0efdaca9f362a
sh-chae <sh.p.chae@gmail.com>
2021-09-07 10:23:17 +0900

    typo

--- DFS_grouping.py
+++ DFS_grouping.py
@@ -1,14 +1,14 @@
-n, m = map(int, input())
-map = []
+n, m = map(int, input().split())
+ma = []
 for i in range(n):
-    map.append(list(map(int, input())))
+    ma.append(list(map(int, input())))
 
 
 def dfs(x, y):
-    if x <= -1 or x >= n or y <= -1 or y >= n:
+    if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
-    if map[x][y] == 0:
-        map[x][y] = 1
+    if ma[x][y] == 0:
+        ma[x][y] = 1
         dfs(x+1, y)
         dfs(x-1, y)
         dfs(x, y+1)
