2c2bd5e23b4600b84a3d5fb94fd480c46cfb8ff5
sh-chae <sh.p.chae@gmail.com>
2021-09-07 10:02:24 +0900

    DFS

---
+++ DFS_grouping.py
@@ -0,0 +1,26 @@
+n, m = map(int, input())
+map = []
+for i in range(n):
+    map.append(list(map(int, input())))
+
+
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= n:
+        return False
+    if map[x][y] == 0:
+        map[x][y] = 1
+        dfs(x+1, y)
+        dfs(x-1, y)
+        dfs(x, y+1)
+        dfs(x, y-1)
+        return True
+    return False
+
+
+result = 0
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j) == True:
+            result += 1
+
+print(result)
