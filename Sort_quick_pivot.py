f5f58745868b32002c9c7da66f48ddc727fc2406
sh-chae <sh.p.chae@gmail.com>
2021-09-11 11:03:49 +0900

    quick sort as python implimentation

--- Sort_quick_pivot.py
+++ Sort_quick_pivot.py
@@ -2,25 +2,17 @@
 array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 
 
-def quick_sort(array, start, end):
-    if start >= end:
-        return
-    pivot = start
-    left = start+1
-    right = end
-    while left <= right:
-        while left <= end and array[left] <= array[pivot]:
-            left += 1
-        while right < start and array[right] >= array[pivot]:
-            right -= 1
-        if left > right:
-            array[right], array[pivot] = array[pivot], array[right]
-        else:
-            array[left], array[right] = array[right], array[left]
-    quick_sort(array, start, right-1)
-    quick_sort(array, right+1, end)
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+
+    pivot = array[0]
+    tail = array[1:]
+
+    left = [x for x in tail if x <= pivot]
+    right = [x for x in tail if x > pivot]
+
+    return quick_sort(left)+[pivot]+quick_sort(right)
 
 
-quick_sort(array, 0, len(array)-1)
-print(array)
- 
+print(quick_sort(array))
